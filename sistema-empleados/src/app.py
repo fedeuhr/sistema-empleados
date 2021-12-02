c03191c8b174b6f97afa280981fb3b81b1d09905
fedeuhr <federicouhr@gmail.com>
2021-12-01 23:13:33 -0300

    Conexión a mysql y test

--- sistema-empleados/src/app.py
+++ sistema-empleados/src/app.py
@@ -9,6 +9,26 @@ app = Flask(__name__)
 #Instanciamos (el nombre de mi app)
 mysql = MySQL()
 
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '3085'
+app.config['MYSQL_DATABASE_DB'] = 'empleados'
+
+mysql.init_app(app) #inicio la DB
+
+@app.route('/')
+
+def index():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    sql="insert into empleados (nombre, correo, foto) values ('Juan', 'juan@email.com', 'fotoJuan.jpg');"
+
+    cursor.execute(sql)
+
+    conn.commit()
+
+    return render_template('empleados/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True) #El debug es para que nos muestre por consola errores
